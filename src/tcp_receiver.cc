f692eddad35553faa846ee83104a2118b7aaf113
DengY11 <212294929@qq.com>
2025-04-23 00:12:36 +0800

    pass check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -5,14 +5,49 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+
+    if ( message.RST ) {
+        this->reassembler_.reader().set_error();
+    }
+
+    std::string&& payload = std::move( message.payload );
+
+    bool SYN = message.SYN;
+
+    if ( SYN && !this->syn_received_ ) {
+        this->zero_point_ = message.seqno;
+        syn_received_ = true;
+    }
+
+    if ( !syn_received_ ) {
+        return;
+    }
+
+    uint64_t check_point = this->reassembler_.writer().bytes_pushed();
+
+    uint64_t abs_seqno = message.seqno.unwrap( this->zero_point_, check_point );
+
+    size_t index = SYN ? 0 : ( abs_seqno - 1 );
+
+    this->reassembler_.insert( index, std::move( payload ), message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+    TCPReceiverMessage message;
+    if ( !this->syn_received_ ) {
+        message.ackno = std::nullopt;
+    } else {
+        uint64_t next_abs = reassembler_.writer().bytes_pushed() + 1;
+        if ( reassembler_.writer().is_closed() ) {
+            next_abs += 1;
+        }
+        message.ackno = Wrap32::wrap( next_abs, zero_point_ );
+    }
+
+    std::size_t capacity = this->reassembler_.writer().available_capacity();
+    message.window_size = static_cast<uint16_t>( std::min( capacity, static_cast<size_t>( UINT16_MAX ) ) );
+
+    message.RST = reassembler_.writer().has_error();
+    return message;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,53 @@
 #include "wrapping_integers.hh"
+#include <vector>
+#include <algorithm>
 #include "debug.hh"
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+    constexpr uint64_t mod_value = static_cast<uint64_t>( 1 ) << 32;
+    return Wrap32 { static_cast<uint32_t>( ( ( n + static_cast<uint64_t>( zero_point.raw_value_ ) ) % mod_value ) ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+    uint32_t offset = static_cast<uint32_t>( this->raw_value_ - zero_point.raw_value_ );
+
+    constexpr uint64_t mod_value = static_cast<uint64_t>( 1 ) << 32;
+
+    uint64_t base = static_cast<uint64_t>( checkpoint / mod_value ) * mod_value + offset;
+
+    uint64_t candidate_1 = base;
+    uint64_t candidate_2 = base + mod_value;
+    uint64_t candidate_3 = base - mod_value;
+
+    auto get_absolute_difference = [&checkpoint]( uint64_t value ) {
+        if ( value >= checkpoint ) {
+            return static_cast<uint64_t>( value - checkpoint );
+        } else {
+            return static_cast<uint64_t>( checkpoint - value );
+        }
+    };
+
+    uint64_t absolute_difference_1 = get_absolute_difference( candidate_1 );
+    uint64_t absolute_difference_2 = get_absolute_difference( candidate_2 );
+    uint64_t absolute_difference_3 = get_absolute_difference( candidate_3 );
+
+    uint64_t best_distance = absolute_difference_1;
+    uint64_t best_candidate = candidate_1;
+
+    if( absolute_difference_2 <= best_distance ) {
+        best_distance = absolute_difference_2;
+        best_candidate = candidate_2;
+    }
+
+    if( absolute_difference_3 <= best_distance ) {
+        best_distance = absolute_difference_3;
+        best_candidate = candidate_3;
+    }
+
+    return best_candidate;
+
 }
